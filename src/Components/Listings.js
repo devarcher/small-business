2b71f4e829b08e86e44f03147c8a1a0e204c4b83
devarcher <archershot@hotmail.com>
2020-02-27 13:27:30 -0600

    conditionally rendering delete on listings

--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -10,6 +10,8 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
+import { checkAuth } from "../Router";
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650
@@ -26,6 +28,9 @@ export default function Listings() {
   const classes = useStyles();
 
   return (
+    <>
+
+    {!checkAuth() ? (
     <Box display='flex' justifyContent='center' padding={8}>
       <Box width='80vw'>
         <TableContainer component={Paper}>
@@ -55,5 +60,42 @@ export default function Listings() {
         </TableContainer>
       </Box>
     </Box>
+    ) : (
+
+          <Box display='flex' justifyContent='center' padding={8}>
+      <Box width='80vw'>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Names</TableCell>
+                <TableCell align="right">Description</TableCell>
+                <TableCell align="right">Hours</TableCell>
+                <TableCell align="right">Address</TableCell>
+                <TableCell align="right">Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                  <TableCell align="right">X</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+
+    
+    )}
+    </>
   );
 }
